373bee82a58083c98012e5e7bc7d3077fc6e82d4
SrNorlan07 <gnorlan654@gmail.com>
2024-04-23 15:27:43 -0600

    first commit

---
+++ public/js/ScriptAbonos.js
@@ -0,0 +1,23 @@
+//MOSTRAR FORMULARIO PARA REALIZAR ABONOS
+function mostrarPanelAbono() {
+    var panel = document.getElementById("panelAbono");
+    panel.style.display = "block";
+  }
+
+  function cerrarPanelAbono() {
+    var panel = document.getElementById("panelAbono");
+    panel.style.display = "none";
+  }
+  //FIN FORMULARIO
+
+  //MOSTAR HISTORIAL DE ABONOS
+  function mostrarPanelHistorialAbonos() {
+    var panel = document.getElementById("panelHistorialAbonos");
+    panel.style.display = "block";
+  }
+
+  function cerrarPanelHistorialAbonos() {
+    var panel = document.getElementById("panelHistorialAbonos");
+    panel.style.display = "none";
+  }
+  //FIN MOSTRAR HISTORIAL
