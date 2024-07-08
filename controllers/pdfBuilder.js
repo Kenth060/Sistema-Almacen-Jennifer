c5fb2cca361c608093e1ecc8d337acc7dc4b36dc
Kenth060 <133837320+Kenth060@users.noreply.github.com>
2024-07-08 15:24:10 -0600

    Commit Merge

--- controllers/pdfBuilder.js
+++ controllers/pdfBuilder.js
@@ -26,6 +26,7 @@ function buildReporteVentas(dataCallback, endCallback , Datos )
         
         // Línea separadora
         doc.moveTo(0, 100)
+        .lineWidth(2)
         .lineTo(700, 100)
         .strokeColor('#077E8B')
         .stroke();
@@ -36,18 +37,22 @@ function buildReporteVentas(dataCallback, endCallback , Datos )
 
     const addTable = async (title, data) => 
     {   
-        doc.font('Times-Bold').fontSize(14).text(`• ${title}`, { bulletRadius: 5 }).moveDown(0.5);
+        doc.font('Times-Bold').fontSize(14).text(`${title}`, { bulletRadius: 5 }).moveDown(0.5);
+
+        const totalVendido = data.reduce((sum, row) => sum + parseFloat(row[2].replace('C$ ', '')), 0);
+        const footer= ['', 'Total ', `C$ ${totalVendido.toFixed(2)}`];
+        data.push(footer);
 
         // Configuración de la tabla
         const table = 
         {
             title: ``, 
             headers: [
-                { label: 'Producto', align: 'center', headerColor: '#28A745', color: '#FFFFFF' },
+                { label: 'Producto', align: 'center', headerColor: '#28A745', color: '#FFFFFF'},
                 { label: 'Cantidad Vendida', align: 'center', headerColor: '#28A745', color: '#FFFFFF' },
                 { label: 'Ingresos totales', align: 'center', headerColor: '#28A745', color: '#FFFFFF' }
             ],
-            rows: data
+            rows: data,
         };
 
         // Dibujar la tabla
@@ -74,6 +79,80 @@ function buildReporteVentas(dataCallback, endCallback , Datos )
     addTable('Ingresos Totales', mapearProductos(Datos[3]) ); 
 
     doc.end();
-}
+} 
+
+function buildReporteVentas(dataCallback, endCallback , Datos )
+{
+    const doc = new PDFDocument();
+
+    doc.on('data',dataCallback);
+    doc.on('end',endCallback);
+
+    doc.font('Times-Bold');
+
+    const table = {
+        title: { label: 'Informe de Ventas al Contado', fontSize: 16, fontFamily: 'Times-Bold' },
+        subtitle: { label: 'Mes de Julio',  fontSize: 13,fontFamily: 'Times-Roman' },
+        headers: [
+          { label: "Productos", property: 'name', width: 60, renderer: null },
+          { label: "Cantidad Vendida", property: 'description', width: 150, renderer: null }, 
+          { label: "Ingresos Totales", property: 'price1', width: 100, renderer: null }, 
+          { label: "Price 2", property: 'price2', width: 100, renderer: null }, 
+          { label: "Price 3", property: 'price3', width: 80, renderer: null }, 
+          { label: "Price 4", property: 'price4', width: 43, 
+            renderer: (value, indexColumn, indexRow, row, rectRow, rectCell) => { return `U$ ${Number(value).toFixed(2)}` } 
+          },
+        ],
+        // complex data
+        datas: [
+          { 
+            name: 'Name 1', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ante in laoreet egestas. ', 
+            price1: '$1', 
+            price3: '$ 3', 
+            price2: '$2', 
+            price4: '4', 
+          },
+          { 
+            options: { fontSize: 10, separation: true},
+            name: 'bold:Name 2', 
+            description: 'bold:Lorem ipsum dolor.', 
+            price1: 'bold:$1', 
+            price3: { 
+              label: 'PRICE $3', options: { fontSize: 12 } 
+            }, 
+            price2: '$2', 
+            price4: '4', 
+          },
+          // {...},
+        ],
+        // simeple data
+        rows: [
+          [
+            "Apple",
+            "Nullam ut facilisis mi. Nunc dignissim ex ac vulputate facilisis.",
+            "$ 105,99",
+            "$ 105,99",
+            "$ 105,99",
+            "105.99",
+          ],
+          // [...],
+        ],
+      };
+
+      // the magic
+      doc.table(table, {
+        prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8),
+        prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+          doc.font("Times-Roman").fontSize(8);
+          indexColumn === 0 && doc.addBackground(rectRow, 'blue', 0.15);
+        },
+      });
+      // done!
+      doc.end();
+    
+
+} 
+    
 
 module.exports = buildReporteVentas;
